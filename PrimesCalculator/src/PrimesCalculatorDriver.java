6aefa48fec5a9ae06cf00d33d1832b97013d7641
Gad Maor <gadm@il.ibm.com>
2019-01-12 22:33:11 +0200

    Done?

--- PrimesCalculator/src/PrimesCalculatorDriver.java
+++ PrimesCalculator/src/PrimesCalculatorDriver.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /** This program calculates and prints all prime numbers from 2 to a given number,
  * using a given number of concurrently running multithreads.
  *  mmn15-1
@@ -10,8 +12,9 @@ public class PrimesCalculatorDriver {
         int maxNumberOfRunningThreads = -1;
         // The number until which to calculate the primes
         int numberToCalculateTo = -1;
-        maxNumberOfRunningThreads = getData("Maximum number of running thread");
-        numberToCalculateTo = getData("The number until which to find prime numbers");
+        Scanner input = new Scanner(System.in);
+        maxNumberOfRunningThreads = getData("maximum number of running thread", input);
+        numberToCalculateTo = getData("The number until which to find prime numbers", input);
         // The thread controller to use for calculating the primes
         PrimesCalculatorController cont = new PrimesCalculatorController(maxNumberOfRunningThreads, numberToCalculateTo);
         for (int i=2; i < numberToCalculateTo; ++i) {
@@ -24,9 +27,36 @@ public class PrimesCalculatorDriver {
         }
         // Wait for all threads to finish their calculations
         cont.waitForLastThread();
-        System.out.println("The prime numbers between 2 and %d");
+        System.out.println("The prime numbers between 2 and %d", numberToCalculateTo);
+        System.out.println(cont.getResults());
     }
-    public static int getData(String data) {
 
+    /** This method checks that the user inputs a positive integer as data.
+     *
+     * @param data A message asking for a specific type of data
+     * @param input The Scanner input object to get input from the user
+     * @return The user's entered integer
+     */
+    private static int getData(String data, Scanner input) {
+        boolean correctData = false;
+        int userInput = 0;
+        do {
+            System.out.println("Please input %s as a positive integer: ", data);
+            try {
+                String nextToken = input.next();
+                userInput = Integer.parseInt(nextToken);
+            }
+            catch (NumberFormatException e) {
+                System.out.println("Invalid data. Must input an integer!");
+                continue;
+            }
+            if (userInput <= 2) {
+                System.out.println("Invalid data. Must input a positive number!");
+                continue;
+            }
+            // If we've reached here, means the number is a positive integer
+            correctData = true;
+        } while(!correctData);
+        return userInput;
     }
 }
